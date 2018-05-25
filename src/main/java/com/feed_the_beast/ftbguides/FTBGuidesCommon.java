db4aad7c522561536db3755b7bff9b911ed9badc
LatvianModder <latvianmodder@gmail.com>
2018-05-25 14:11:39 +0300

    Fixed server guide page loading

--- src/main/java/com/feed_the_beast/ftbguides/FTBGuidesCommon.java
+++ src/main/java/com/feed_the_beast/ftbguides/FTBGuidesCommon.java
@@ -5,6 +5,7 @@ import com.feed_the_beast.ftblib.lib.io.DataReader;
 import com.feed_the_beast.ftblib.lib.util.CommonUtils;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonNull;
+import com.google.gson.JsonObject;
 
 import java.io.File;
 import java.util.HashMap;
@@ -13,6 +14,7 @@ import java.util.Map;
 public class FTBGuidesCommon
 {
 	public static JsonElement serverGuide = null;
+	private static final Map<String, File> LOADED_PAGE_PATHS = new HashMap<>();
 	private static final Map<String, JsonElement> LOADED_PAGES = new HashMap<>();
 
 	public void preInit()
@@ -32,44 +34,97 @@ public class FTBGuidesCommon
 	public static void reloadServerGuide()
 	{
 		serverGuide = null;
-		LOADED_PAGES.clear();
 	}
 
 	public static JsonElement getServerGuide()
 	{
 		if (serverGuide == null)
 		{
-			serverGuide = DataReader.get(new File(CommonUtils.folderLocal, "server_guide/data.json")).safeJson();
+			File folder = new File(CommonUtils.folderLocal, "server_guide");
+			serverGuide = DataReader.get(new File(folder, "data.json")).safeJson();
+			LOADED_PAGE_PATHS.clear();
+			LOADED_PAGES.clear();
+
+			if (serverGuide.isJsonObject() && serverGuide.getAsJsonObject().has("pages"))
+			{
+				JsonElement pages0 = serverGuide.getAsJsonObject().get("pages");
+
+				if (pages0.isJsonArray())
+				{
+					for (JsonElement element : pages0.getAsJsonArray())
+					{
+						if (element.isJsonObject() && element.getAsJsonObject().has("id"))
+						{
+							loadPagePath(folder, "/", element.getAsJsonObject());
+						}
+					}
+				}
+			}
 		}
 
 		return serverGuide;
 	}
 
+	private static void loadPagePath(File folder, String path, JsonObject json)
+	{
+		String id = json.get("id").getAsString();
+		String pagePath = path + id + "/"; // /rules/
+
+		File file = new File(folder, pagePath + "index.json");
+
+		if (!file.exists())
+		{
+			file = new File(folder, pagePath.substring(0, pagePath.length() - 1) + ".json");
+		}
+
+		if (file.exists() && file.getAbsolutePath().startsWith(folder.getAbsolutePath()))
+		{
+			LOADED_PAGE_PATHS.put(pagePath, file);
+		}
+		else
+		{
+			FTBGuides.LOGGER.error("Couldn't load server guide page " + pagePath + "!");
+		}
+
+		if (json.has("pages"))
+		{
+			JsonElement pages0 = json.get("pages");
+
+			if (pages0.isJsonArray())
+			{
+				for (JsonElement element : pages0.getAsJsonArray())
+				{
+					if (element.isJsonObject() && element.getAsJsonObject().has("id"))
+					{
+						loadPagePath(folder, pagePath, element.getAsJsonObject());
+					}
+				}
+			}
+		}
+	}
+
 	public static JsonElement getLoadedPage(String path)
 	{
+		getServerGuide();
 		JsonElement json = LOADED_PAGES.get(path);
 
 		if (json == null)
 		{
-			File folder = new File(CommonUtils.folderLocal, "server_guide");
-			File file = new File(folder, path + ".json");
+			json = JsonNull.INSTANCE;
 
-			if (!file.exists())
-			{
-				file = new File(folder, path + ".json");
-			}
+			File file = LOADED_PAGE_PATHS.get(path);
 
-			if (file.getAbsolutePath().startsWith(folder.getAbsolutePath()))
+			if (file != null)
 			{
 				json = DataReader.get(file).safeJson();
+
+				if (!json.isJsonArray())
+				{
+					json = JsonNull.INSTANCE;
+				}
 			}
 
 			LOADED_PAGES.put(path, json);
-		}
-
-		if (json == null || !json.isJsonArray())
-		{
-			json = JsonNull.INSTANCE;
 		}
 
 		return json;
