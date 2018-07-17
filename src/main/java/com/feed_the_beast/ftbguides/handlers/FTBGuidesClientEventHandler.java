3da96063d184fa43b5768dac9fdef8f2c69f2d76
LatvianModder <latvianmodder@gmail.com>
2018-07-17 13:24:55 +0300

    Updated mappings

--- src/main/java/com/feed_the_beast/ftbguides/handlers/FTBGuidesClientEventHandler.java
+++ src/main/java/com/feed_the_beast/ftbguides/handlers/FTBGuidesClientEventHandler.java
@@ -73,9 +73,9 @@ public class FTBGuidesClientEventHandler
 	@SubscribeEvent
 	public static void onCustomClick(CustomClickEvent event)
 	{
-		if (event.getID().getResourceDomain().equals(FTBGuides.MOD_ID))
+		if (event.getID().getNamespace().equals(FTBGuides.MOD_ID))
 		{
-			switch (event.getID().getResourcePath())
+			switch (event.getID().getPath())
 			{
 				case "reload":
 					FTBGuidesClient.setShouldReload();
