15f65906ba00090f4b61c93300eb266f3e2db392
LatvianModder <latvianmodder@gmail.com>
2018-10-31 20:26:22 +0200

    FTBLib changes

--- src/main/java/com/feed_the_beast/ftbguides/handlers/FTBGuidesClientEventHandler.java
+++ src/main/java/com/feed_the_beast/ftbguides/handlers/FTBGuidesClientEventHandler.java
@@ -4,12 +4,14 @@ import com.feed_the_beast.ftbguides.FTBGuides;
 import com.feed_the_beast.ftbguides.FTBGuidesConfig;
 import com.feed_the_beast.ftbguides.client.FTBGuidesClient;
 import com.feed_the_beast.ftbguides.client.FTBGuidesClientConfig;
-import com.feed_the_beast.ftblib.events.CustomSidebarButtonTextEvent;
+import com.feed_the_beast.ftblib.events.SidebarButtonCreatedEvent;
 import com.feed_the_beast.ftblib.events.client.CustomClickEvent;
 import com.feed_the_beast.ftblib.events.client.GuideEvent;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.init.Items;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
@@ -26,14 +28,25 @@ public class FTBGuidesClientEventHandler
 	private static final ResourceLocation GUIDES_BUTTON = new ResourceLocation(FTBGuides.MOD_ID, "guides");
 
 	@SubscribeEvent
-	public static void onCustomSidebarButtonText(CustomSidebarButtonTextEvent event)
+	public static void onSidebarButtonCreated(SidebarButtonCreatedEvent event)
 	{
-		if (FTBGuidesConfig.general.flash_guides && !FTBGuidesConfig.general.modpack_guide_version.isEmpty() && event.getButton().id.equals(GUIDES_BUTTON))
+		if (event.getButton().id.equals(GUIDES_BUTTON))
 		{
-			if (System.currentTimeMillis() % 1000L >= 500L && !FTBGuidesClientConfig.general.last_guide_version.equals(FTBGuidesConfig.general.modpack_guide_version))
-			{
-				event.setText(" ! ");
-			}
+			event.getButton().setCustomTextHandler(() -> {
+				if (FTBGuidesConfig.general.flash_guides && !FTBGuidesConfig.general.modpack_guide_version.isEmpty() && System.currentTimeMillis() % 1000L >= 500L && !FTBGuidesClientConfig.general.last_guide_version.equals(FTBGuidesConfig.general.modpack_guide_version))
+				{
+					return "[!]";
+				}
+
+				return "";
+			});
+
+			event.getButton().setTooltipHandler(list -> {
+				if (FTBGuidesConfig.general.flash_guides && !FTBGuidesConfig.general.modpack_guide_version.isEmpty() && !FTBGuidesClientConfig.general.last_guide_version.equals(FTBGuidesConfig.general.modpack_guide_version))
+				{
+					list.add(TextFormatting.GRAY + I18n.format("sidebar_button.ftbguides.guides.new_version"));
+				}
+			});
 		}
 	}
 
