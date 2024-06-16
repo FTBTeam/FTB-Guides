cfd4df8f089c79de7d4a1a8a16a97e47f7cbfe09
Michael Hillcox <michaelhillcox@gmail.com>
2024-06-16 19:00:28 +0100

    fix: neoforge issues & creative tab issues

--- common/src/main/java/dev/ftb/mods/ftbguides/registry/GuideBookItem.java
+++ common/src/main/java/dev/ftb/mods/ftbguides/registry/GuideBookItem.java
@@ -5,7 +5,7 @@ import net.minecraft.world.item.Item;
 
 public class GuideBookItem extends Item {
     public GuideBookItem() {
-        super(new Properties().stacksTo(1).arch$tab(ModItems.ITEM_GROUP.get())); // I don't like this arch!
+        super(new Properties().stacksTo(1));
     }
 
 }
--- common/src/main/java/dev/ftb/mods/ftbguides/registry/ModItems.java
+++ common/src/main/java/dev/ftb/mods/ftbguides/registry/ModItems.java
@@ -2,6 +2,7 @@ package dev.ftb.mods.ftbguides.registry;
 
 import dev.architectury.registry.CreativeTabRegistry;
 import dev.architectury.registry.registries.DeferredRegister;
+import dev.architectury.registry.registries.RegistrarManager;
 import dev.architectury.registry.registries.RegistrySupplier;
 import dev.ftb.mods.ftbguides.FTBGuides;
 import net.minecraft.core.component.DataComponentType;
@@ -12,7 +13,6 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
 public class ModItems {
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_TAB = DeferredRegister.create(FTBGuides.MOD_ID, Registries.CREATIVE_MODE_TAB);
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(FTBGuides.MOD_ID, Registries.ITEM);
     public static final DeferredRegister<DataComponentType<?>> COMPONENTS = DeferredRegister.create(FTBGuides.MOD_ID, Registries.DATA_COMPONENT_TYPE);
 
@@ -21,14 +21,18 @@ public class ModItems {
             .persistent(GuideBookData.CODEC)
             .build());
 
-    public static final RegistrySupplier<CreativeModeTab> ITEM_GROUP = CREATIVE_TAB.register("ftbguides", () -> CreativeTabRegistry.create(
-            Component.translatable("itemGroup.ftbguides.ftbguides"), () -> new ItemStack(ModItems.BOOK.get())
-    ));
+    public static final RegistrySupplier<CreativeModeTab> CREATIVE_TAB = RegistrarManager.get(FTBGuides.MOD_ID)
+            .get(Registries.CREATIVE_MODE_TAB)
+            .register(FTBGuides.rl("default"), () -> CreativeTabRegistry.create(builder -> {
+                builder.title(Component.translatable("itemGroup.ftbguides.ftbguides"))
+                    .icon(() -> new ItemStack(ModItems.BOOK.get()))
+                    .displayItems((params, output) -> {
+                        output.accept(new ItemStack(ModItems.BOOK.get()));
+                    });
+            }));
 
     public static void register() {
-        CREATIVE_TAB.register();
         COMPONENTS.register();
         ITEMS.register();
     }
-
 }
